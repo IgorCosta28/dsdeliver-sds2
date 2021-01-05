ef823c38802c38d9200e55a427ad8db20f05e265
Igor Costa <igordacostasilva18@gmail.com>
2021-01-04 21:10:27 -0300

    Project Created

---
+++ backEnd/src/main/java/com/devsuperior/sdDeliver/SdDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdDeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdDeliverApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/devsuperior/sdDeliver/SdDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdDeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
